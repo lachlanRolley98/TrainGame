169b1c91b5c277e753c2fe19454a3add6b796e5c
Lachlan Rolley <lachlan.rolley@student.unsw.edu.au>
2023-01-07 16:50:03 +1100

    Just creating starter game file

--- README.md
+++ README.md
@@ -1,2 +1,3 @@
 # TrainGame
 Making the train game code, will put in app later
+just setting up github
---
+++ game.c
@@ -0,0 +1,7 @@
+#include <stdio.h>
+
+int main(){
+
+    printf("insert train game here ?\n");
+    return 0;
+}
