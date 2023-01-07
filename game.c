ea9072709068350cc0eb2a15df9015035bb4a149
Lachlan Rolley <lachlan.rolley@student.unsw.edu.au>
2023-01-07 17:28:36 +1100

    wrote ideas

--- game.c
+++ game.c
@@ -2,6 +2,15 @@
 
 int main(){
 
-    printf("insert train game here ?\n");
+   /*
+   RULES:
+        use all 4 numbers in order (bodmas) to create 10
+        there are only -, +, *, /, ^, (skip route cbs)
+        1) use if and loops to run through all combinations then print it out
+        2) use recursion to run through all combos then print
+        3) linked lists so can create heaps long arrays (or just have heaps long arrays)
+        4) might have to do the table shit on search trees to remember how the code got to that number
+   */
+
     return 0;
 }
